3943f2030257d6114b18a9c8b190b5d5c6d1e694
Radek <radek@MacBook-Pro-Radek.local>
2020-04-19 23:52:31 +0200

    Updates

---
+++ precache-manifest.af54c4c93e281e334650d2ae3ea25515.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e5142f6c40ec1c65c41fa7dcd86062",
+    "url": "/react-boilerplate/index.html"
+  },
+  {
+    "revision": "928495e99ca36f1ffb09",
+    "url": "/react-boilerplate/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a9cf297eac3f78555713",
+    "url": "/react-boilerplate/static/js/2.97902970.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-boilerplate/static/js/2.97902970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "928495e99ca36f1ffb09",
+    "url": "/react-boilerplate/static/js/main.a4b2baff.chunk.js"
+  },
+  {
+    "revision": "083dc959955f5422e43c",
+    "url": "/react-boilerplate/static/js/runtime-main.4a8da873.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-boilerplate/static/media/logo.5d5d9eef.svg"
+  }
+]);
